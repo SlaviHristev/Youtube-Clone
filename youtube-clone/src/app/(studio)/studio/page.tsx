f35cea1082484c2dbfb2dc0ebbef8f2e51456aa9
Slavi Hristev <Slavihristev97@gmail.com>
2025-03-27 17:57:20 +0200

    Continue working on the videos page

--- youtube-clone/src/app/(studio)/studio/page.tsx
+++ youtube-clone/src/app/(studio)/studio/page.tsx
@@ -1,8 +1,11 @@
 import { StudioView } from "@/app/modules/studio/ui/view/studio-view";
+import { DEFAULT_LIMIT } from "@/constants";
 import { HydrateClient, trpc } from "@/trpc/server";
 
 const Page = async () =>{
-    void trpc.studio.getMany.prefetchInfinite();
+    void trpc.studio.getMany.prefetchInfinite({
+        limit: DEFAULT_LIMIT,
+    });
     return(
         <HydrateClient>
             <StudioView/>
--- youtube-clone/src/trpc/routers/_app.ts
+++ youtube-clone/src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
 import { categoriesRouter } from "@/app/modules/categories/server/precedures";
 import { createTRPCRouter } from "../init";
 import { StudioRouter } from "@/app/modules/studio/server/procedures";
+import { videosRouter } from "@/app/modules/videos/server/procedures";
 export const appRouter = createTRPCRouter({
   studio: StudioRouter,
+  videos: videosRouter,
   categories: categoriesRouter,
 });
 
