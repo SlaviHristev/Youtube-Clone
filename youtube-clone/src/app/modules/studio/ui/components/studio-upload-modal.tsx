8a86e2c109a78d3da07effda3009ba0712dcd209
Slavi Hristev <Slavihristev97@gmail.com>
2025-04-02 18:12:15 +0300

    Integrating mux for videos uploading and processing

--- youtube-clone/src/app/modules/studio/ui/components/studio-upload-modal.tsx
+++ youtube-clone/src/app/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,34 +1,51 @@
-"use client"
+"use client";
 
-import { ResponsiveModal } from "@/components/responsive-modal"
-import { Button } from "@/components/ui/button"
-import { trpc } from "@/trpc/client"
-import { Loader2Icon, PlusIcon } from "lucide-react"
-import { toast } from "sonner"
+import { ResponsiveModal } from "@/components/responsive-modal";
+import { Button } from "@/components/ui/button";
+import { trpc } from "@/trpc/client";
+import { Loader2Icon, PlusIcon } from "lucide-react";
+import { toast } from "sonner";
+import { StudioUploader } from "./studio-uploader";
 
-export const StudioUploadModal = () =>{
-    const utils = trpc.useUtils();
-    const create = trpc.videos.create.useMutation({
-        onSuccess: () =>{
-            toast.success("Video created")
-            utils.studio.getMany.invalidate();
-        },
-        onError: (error) => {
-            toast.error(error.message)
-        }
-    });
+export const StudioUploadModal = () => {
+  const utils = trpc.useUtils();
+  const create = trpc.videos.create.useMutation({
+    onSuccess: () => {
+      toast.success("Video created");
+      utils.studio.getMany.invalidate();
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
 
-    return(
-        <>
-        <ResponsiveModal title="Upload a video" open={!!create.data} onOpenChange={() => create.reset()}>
-            <p >
-                Uploader
-            </p>
-        </ResponsiveModal>
-        <Button variant='secondary' onClick={() => create.mutate()} disabled={create.isPending}> 
-            {create.isPending ? <Loader2Icon className="animate-spin"/> : <PlusIcon />}
-            Create
-        </Button>
-        </>
-    )
-}
+  return (
+    <>
+      <ResponsiveModal
+        title="Upload a video"
+        open={!!create.data?.url}
+        onOpenChange={() => create.reset()}
+      >
+        <p>
+          {create.data?.url ? (
+            <StudioUploader endpoint={create.data.url} onSuccess={() =>{}}/>
+          ) : (
+            <Loader2Icon />
+          )}
+        </p>
+      </ResponsiveModal>
+      <Button
+        variant="secondary"
+        onClick={() => create.mutate()}
+        disabled={create.isPending}
+      >
+        {create.isPending ? (
+          <Loader2Icon className="animate-spin" />
+        ) : (
+          <PlusIcon />
+        )}
+        Create
+      </Button>
+    </>
+  );
+};
