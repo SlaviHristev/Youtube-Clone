abcff1879aec9ef7b1a71a6e0fb5698589f8f6a4
Slavi Hristev <Slavihristev97@gmail.com>
2025-05-16 18:07:52 +0300

    Implemented video viewing logic when watching a video

--- youtube-clone/src/trpc/routers/_app.ts
+++ youtube-clone/src/trpc/routers/_app.ts
@@ -2,10 +2,12 @@ import { categoriesRouter } from "@/app/modules/categories/server/precedures";
 import { createTRPCRouter } from "../init";
 import { StudioRouter } from "@/app/modules/studio/server/procedures";
 import { videosRouter } from "@/app/modules/videos/server/procedures";
+import { videoViewsRouter } from "@/app/modules/video-views/server/procedures";
 export const appRouter = createTRPCRouter({
   studio: StudioRouter,
   videos: videosRouter,
   categories: categoriesRouter,
+  videoViews: videoViewsRouter,
 });
 
 export type AppRouter = typeof appRouter;
